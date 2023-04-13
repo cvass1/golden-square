e0c3a05872d0d0a3df3de73522010e3878f2f1cd
Caroline Evans <carolineevans261@gmail.com>
2023-04-13 10:43:57 +0100

    changing folder structure

--- lib/greet.rb
+++
@@ -1,5 +0,0 @@
-def greet(name)
-    return "Hello, #{name}"
-end
-
-# Carolines code - testing github
--- spec/greet_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'greet'
-
-RSpec.describe "testing greet method" do
-  it "returns 'Hello, Mary' when given 'Mary'" do
-    expect(greet("Mary")).to eq "Hello, Mary"
-  end
-end
