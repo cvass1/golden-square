19ba6df5a3f635b6b0adb67ea44c1368063b4422
Caroline Evans <carolineevans261@gmail.com>
2023-04-17 12:06:15 +0100

    started section 7

--- phase_two/lib/vowel_remover.rb
+++ phase_two/lib/vowel_remover.rb
@@ -6,12 +6,13 @@ class VowelRemover
 
   def remove_vowels()
     i = 0
+
     while i < @text.length()
-      binding.irb
       if @vowels.include? @text[i].downcase
         @text = @text.slice(0,i) + @text.slice(i+1..-1)
+      else 
+        i += 1
       end
-      i += 1
     end
     return @text
   end
--- phase_two/spec/vowel_remover_spec.rb
+++ phase_two/spec/vowel_remover_spec.rb
@@ -1,6 +1,19 @@
 require 'vowel_remover'
 
 RSpec.describe "remove_vowels method" do
+  
+  it "removes all letters from string 'aeiou' and returns an empty string" do
+    remover = VowelRemover.new("aeiou")
+    result_no_vowels = remover.remove_vowels
+    expect(result_no_vowels).to eq ""
+  end
+
+  it "removes all vowels from string 'aaeebeeeiiibiiobuuuubuuu' and returns an empty string" do
+    remover = VowelRemover.new("aaeebeeeiiibiiobuuuubuuu")
+    result_no_vowels = remover.remove_vowels
+    expect(result_no_vowels).to eq "bbbb"
+  end
+  
   it "removes a vowel from a simple string" do
     remover = VowelRemover.new("ab")
     result_no_vowels = remover.remove_vowels
