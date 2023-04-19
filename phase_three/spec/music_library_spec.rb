5228e5552a763859745f196c3c1ec3691ac622a9
sarahc-dev <sarahclements@hotmail.co.uk>
2023-04-19 14:58:04 +0100

    add music_library unit test

--- phase_three/spec/music_library_spec.rb
+++ phase_three/spec/music_library_spec.rb
@@ -15,6 +15,31 @@ describe MusicLibrary do
       music_library.add(track_2)
       expect(music_library.all).to eq [track_1, track_2]
     end
-    
+  end
+
+  context "for method #search" do
+    it "returns an empty list" do
+      music_library = MusicLibrary.new
+      result = music_library.search("Forever")
+      expect(result).to eq []
+    end
+
+    it "returns a list of tracks that match a search" do
+      music_library = MusicLibrary.new
+      track_1 = double(:fake_track, matches?: true )
+      music_library.add(track_1)
+      result = music_library.search("cats")
+      expect(result).to eq [track_1]
+    end
+
+    it "returns an empty list when no matches" do
+      music_library = MusicLibrary.new
+      track_1 = double(:fake_track, matches?: false )
+      track_2 = double(:fake_track, matches?: false )
+      music_library.add(track_1)
+      music_library.add(track_2)
+      result = music_library.search("cats")
+      expect(result).to eq []
+    end
   end
 end
