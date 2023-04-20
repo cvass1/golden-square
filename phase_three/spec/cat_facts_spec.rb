d6940d3cbf7e9dc5c2d33c4197238d541c7dc9e6
Caroline Evans <carolineevans261@gmail.com>
2023-04-20 16:20:25 +0100

    finished sec4 phase3

--- phase_three/spec/cat_facts_spec.rb
+++ phase_three/spec/cat_facts_spec.rb
@@ -1,1 +1,15 @@
 require 'cat_facts'
+
+RSpec.describe CatFacts do
+  it "returns a cat fact" do
+    fake_requester = double(:fake_requester)
+    expect(fake_requester).to receive(:get)
+    .with(URI("https://catfact.ninja/fact"))
+    .and_return('{"fact":"Ginger tabby cats can have freckles around their mouths and on their eyelids!","length":77}')
+    cat_fact = CatFacts.new(fake_requester)
+    result = cat_fact.provide
+    expect(result).to eq "Cat fact: Ginger tabby cats can have freckles around their mouths and on their eyelids!"
+  end
+end
+
+
